06b954aa40cedd4624ca86ba5226405ca7298647
marianogutierrez2008 <165956853+marianogutierrez2008@users.noreply.github.com>
2025-04-26 12:42:12 -0300

    Update TaskItem.jsx

--- mariano aportes/src/componentes/TaskItem.jsx
+++ mariano aportes/src/componentes/TaskItem.jsx
@@ -1,1 +1,15 @@
+import React from "react";
 
+function TaskItem({task, ontoggle, ondelete}){
+    return(
+        <li>
+            
+            <span style={{textDecoration: task.completada ? "line-through" : "none"}}>
+                {task.tarea}
+            </span>
+            <button onClick={ontoggle} style={{marginRight:"15px"}}>Realizada</button>
+            <button onClick={ondelete}>Eliminar</button>
+        </li>
+    )
+}
+export default TaskItem
